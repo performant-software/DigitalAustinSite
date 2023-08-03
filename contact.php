72ae24c138769fe1dc580ed6051a08671edb7d54
Camden Mecklem <c@camdenmecklem.com>
2023-08-03 14:44:22 -0400

    switch to postmark

--- contact.php
+++ contact.php
@@ -1,9 +1,10 @@
 <?php require_once 'src/TemplateRenderer.class.php';
+require_once('vendor/autoload.php');
+use Postmark\PostmarkClient;
+use Postmark\Models\PostmarkException;
 
 $success = null;
-
 if(isset($_POST['submit'])) {
-  require_once('vendor/autoload.php');
   require_once('php/localCredentials.php');
 
   // Google recaptcha site key and secret key
@@ -22,14 +23,33 @@ if(isset($_POST['submit'])) {
     $reason = filter_var($_POST['contact_reason'], FILTER_SANITIZE_STRING);
     $message = filter_var($_POST['contact_message'], FILTER_SANITIZE_STRING);
 
-    $to = "andrew.torget@unt.edu";
-    $subject = "Digital Austin Collection: " . $reason;
-    $body = $message . "\n\n" . "From: " . $name . " - " . $email;
+    try {
+      $client = new PostmarkClient(getenv('POSTMARK_TOKEN'));
+      $subject = "Digital Austin Collection: " . $reason;
+      $body = $message . "\n\n" . "From: " . $name . " - " . $email;
 
-    if (mail($to, $subject, $body)) {
+      $sendResult = $client->sendEmail(getenv('EMAIL_SENDER'),
+        getenv('EMAIL_RECIPIENT'),
+        $subject,
+        $body);
+
+      // Getting the MessageID from the response
+      echo $sendResult->MessageID;
       $success = 'success';
-    }
+    } catch(PostmarkException $ex){
+      // If the client is able to communicate with the API in a timely fashion,
+      // but the message data is invalid, or there's a server error,
+      // a PostmarkException can be thrown.
+      echo $ex->httpStatusCode;
+      echo $ex->message;
+      echo $ex->postmarkApiErrorCode;
 
+    } catch(Exception $generalException){
+      // A general exception is thrown if the API
+      // was unreachable or times out.
+      echo 'Oops! General exception occured while trying to send mail with Postmark:';
+      echo $generalException->getMessage();
+    }
   } else {
     $errors = $resp->getErrorCodes();
   }
